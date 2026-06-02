0a15891cba581a0f084d990edbd008b57833f559
ayoubpro2224 <ayoubpro2224@gmail.com>
2026-06-03 00:58:59 +0100

    Load shared library and update home route response
    Added functionality to load a shared library and check its existence.

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
 import os
+import ctypes
 
 app = Flask(__name__)
 
-print("FILES_IN_PROJECT =", os.listdir("/opt/render/project/src"))
+BASE = os.path.dirname(os.path.abspath(__file__))
+SO_FILE = os.path.join(BASE, "libdecodexmldyluc1.so")
+
+print("SO PATH =", SO_FILE)
+print("EXISTS =", os.path.exists(SO_FILE))
+
+try:
+    lib = ctypes.CDLL(SO_FILE)
+    print("LIBRARY LOADED OK")
+except Exception as e:
+    print("LOAD ERROR =", e)
 
 @app.route("/")
 def home():
-    return "OK"
+    return "Township Decoder Online"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
